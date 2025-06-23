ffb91935b1e61e42e88a8f077ed0b4dfadd19ea4
AryllPetersen <aryll2005@gmail.com>
2025-06-22 22:02:07 -0500

    updated schematic, header, and control unit

--- control_unit.sv
+++ control_unit.sv
@@ -1,14 +1,63 @@
-`include "rv32I.h"
+`default_nettype none
 
 
 module ControlUnit(
   input logic clock, resetn,
-  input logic[31:0] ir,
-  input irsel_t irsel,
-  input pcsel_t pcsel,
-  
+  input ir_R_t ir,
+  input logic AltB, AeqB, AgtB,
 
+  output pcsel_t pcsel,
+  output irsel_t irsel, 
+  output regsel_t regsel,
+  output logic regen,
+  output alusel0_t alusel0,
+  output alusel1_t alusel1,
+  output operations_t op,
+  output logic we, re,
+  output addrsel_t addrsel
 );
+  instruction_t instruction;
+  logic inst_fetched, inst_fetched_d;
+  
+  assign instruction = instruction_t'{
+    opcode: ir.opcode, 
+    funct3: ir.funct3, 
+    funct7: ir.funct7
+  };
 
+  always_comb begin
+    {pcsel, irsel, regsel, regen, alusel0, alusel1,
+       op, we, re, addrsel, inst_fetched_d} = '0;
 
+    inst_fetched_d = 1'b1;
+
+    if(inst_fetched) begin
+      case(instruction) inside
+        {OP, 3'b???, 7'b???????}: begin
+          {pcsel, irsel, addrsel, re, we} = {PCSEL_PLUS4, IRSEL_MEM, ADDRSEL_PC, 1'b1, 1'b0};
+
+          regsel = REGSEL_ALU;
+          regen = 1'b1;
+          alusel0 = ALUSEL0_REG;
+          alusel1 = ALUSEL1_REG;
+          op = ADD; // TODO
+        end          
+        default: begin
+          assert(1'b0)
+            else begin
+              $error("%h is not a valid instruction", instruction);
+            end
+        end
+      endcase
+    end else begin
+      {pcsel, irsel, addrsel, re, we} = {PCSEL_PLUS4, IRSEL_MEM, ADDRSEL_PC, 1'b1, 1'b0};
+    end
+  end
+
+  always_ff @(posedge clock) begin
+    if(~resetn)
+      inst_fetched <= 1'b0;
+    else  
+      inst_fetched <= inst_fetched_d;
+  end
 endmodule
--- rv32I.svh
+++ rv32I.svh
@@ -1,64 +1,77 @@
+typedef enum logic[6:0] {
+  OP      = 7'b0110011,
+  OPIMM   = 7'b0010011,
+  LOAD    = 7'b0000011,
+  STORE   = 7'b0100011,
+  BRANCH  = 7'b1100011,
+  JAL     = 7'b1101111,
+  JALR    = 7'b1100111,
+  LUI     = 7'b0110111,
+  AUIPC   = 7'b0010111
+} opcode_t;
+
 typedef enum logic[1:0] {
-  PC = 2'h0,
-  PLUS4, 2'h1,
-  ALU, 2'h2
+  PCSEL_PC = 2'h0,
+  PCSEL_PLUS4 = 2'h1,
+  PCSEL_ALU = 2'h2
 } pcsel_t;
 
 typedef enum logic {
-  IR = 1'b0,
-  MEM = 1'b1
+  IRSEL_IR = 1'b0,
+  IRSEL_MEM = 1'b1
 } irsel_t;
 
 typedef enum logic[1:0] {
-  ALU = 2'h0,
-  MEM = 2'h1,
+  REGSEL_ALU = 2'h0,
+  REGSEL_MEM = 2'h1,
+  REGSEL_PLUS4 = 2'h2
 } regsel_t;
 
 typedef enum logic {
-  REG = 1'b0,
-  PC = 1'b1,
+  ALUSEL0_REG = 1'b0,
+  ALUSEL0_PC = 1'b1
 } alusel0_t;
 
 typedef enum logic {
-  REG = 1'b0,
-  IR = 1'b1,
+  ALUSEL1_REG = 1'b0,
+  ALUSEL1_IR = 1'b1
 } alusel1_t;
 
 typedef enum logic {
-  PC = 1'b0,
-  ALU = 1'b1,
+  ADDRSEL_PC = 1'b0,
+  ADDRSEL_ALU = 1'b1
 } addrsel_t;
 
 typedef struct packed {
-  logic[6:0] funct7,
-  logic[4:0] rs2,
-  logic[4:0] rs1,
-  logic[2:0] funct3,
-  logic[4:0] rd,
-  logic[6:0] opcode
+  logic[6:0] funct7;
+  logic[4:0] rs2;
+  logic[4:0] rs1;
+  logic[2:0] funct3;
+  logic[4:0] rd;
+  opcode_t  opcode;
 } ir_R_t;
 
 typedef struct packed {
-  logic[11:0] imm,
-  logic[4:0] rs1,
-  logic[2:0] funct3,
-  logic[4:0] rd,
-  logic[6:0] opcode
+  logic[11:0] imm;
+  logic[4:0] rs1;
+  logic[2:0] funct3;
+  logic[4:0] rd;
+  opcode_t  opcode;
 } ir_I_t;
 
 typedef struct packed {
-  logic[6:0] imm1,
-  logic[4:0] rs2,
-  logic[4:0] rs1,
-  logic[2:0] funct3,
-  logic[4:0] imm0,
-  logic[6:0] opcode
+  logic[6:0] imm1;
+  logic[4:0] rs2;
+  logic[4:0] rs1;
+  logic[2:0] funct3;
+  logic[4:0] imm0;
+  opcode_t  opcode;
 } ir_S_t;
 
 typedef struct packed {
-  logic[19:0] imm,
-  logic[4:0] rd,
-  logic[6:0] opcode
+  logic[19:0] imm;
+  logic[4:0] rd;
+  opcode_t opcode;
 } ir_U_t;
 
 typedef enum logic[3:0] {
@@ -74,8 +87,10 @@ typedef enum logic[3:0] {
   SLTU
 } operations_t;
 
-typedef stuct packed {
-  operations_t oper,
-  logic
-} op;
+typedef struct packed {
+  opcode_t opcode;
+  logic[2:0] funct3;
+  logic[6:0] funct7;
+} instruction_t;
+
 
